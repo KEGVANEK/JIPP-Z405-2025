8dd0780430b58b8a5e132894b150fa859ebe533a
Juliusz Kopciński <juliusz.kopcinski@gmail.com>
2025-03-02 12:12:24 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿Console.WriteLine("Podaj swoje imię:");
+var name = Console.ReadLine();
+Console.WriteLine("Hello " + name);
+
+
+int result1 = 5 + 9;
+
+
+int number = 1;
+float money = 1.37f;
+string text =  "hello";
+bool isLogged = true;
+char myChar = 'k';
+decimal price = 6;
+
+
+Console.WriteLine(number);
+Console.WriteLine(money);
+Console.WriteLine(text);
+Console.WriteLine(isLogged);
+Console.WriteLine(myChar);
+Console.WriteLine(price);
+
+string myAge = "Age: ";
+int wifeAge = 18;
+
+string result = myAge + wifeAge;
+Console.WriteLine(result);
+
+bool isTrue = true;
+bool isFalse = false;
+bool isReallyTrue = true;
+
+bool and = isTrue && isFalse; 
+bool or = isTrue || isReallyTrue; 
+bool negative = !isFalse; 
+
+Console.WriteLine("and: " + and);
+Console.WriteLine("or: " + or);
+Console.WriteLine("negative: " + negative);
+
+
+int a = 5;
+int b = 12;
+
+
+int add = a + b; 
+int sub = a - b; 
+int div = a / b; 
+int mul = a * b; 
+int mod = a % b; 
+
+
+Console.WriteLine("add: " + add);
+Console.WriteLine("sub: " + sub);
+Console.WriteLine("div: " + div);
+Console.WriteLine("mul: " + mul);
+Console.WriteLine("mod: " + mod);
+
+
+
+string a2 = "Ala";
+string b2 = "ma";
+string c2 = "kota";
+
+string result2 = a2 + " " + b2 + " " + c2;
+
+Console.WriteLine(result2);
